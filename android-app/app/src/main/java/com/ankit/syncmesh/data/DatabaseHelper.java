cb1d2a3e08b555db15d67ed49ae89e87e5e1107b
Ankit Sharma <153091619+ankitsharma785197@users.noreply.github.com>
2026-05-02 01:42:43 +0530

    Delete android-app directory

--- android-app/app/src/main/java/com/ankit/syncmesh/data/DatabaseHelper.java
+++
@@ -1,9 +0,0 @@
-package com.ankit.syncmesh.data;
-
-import android.content.Context;
-
-public class DatabaseHelper extends SyncDatabaseHelper {
-    public DatabaseHelper(Context context) {
-        super(context);
-    }
-}
--- android-app/app/src/main/java/com/ankit/syncmesh/model/LogEntry.java
+++
@@ -1,9 +0,0 @@
-package com.ankit.syncmesh.model;
-
-public class LogEntry {
-    public long id;
-    public String level;
-    public String tag;
-    public String message;
-    public long createdAt;
-}
