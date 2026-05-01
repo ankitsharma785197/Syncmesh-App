74a689147f38f3b9e92341162379d44a0d3892f0
Ankit Sharma <153091619+ankitsharma785197@users.noreply.github.com>
2026-05-02 01:52:36 +0530

    Delete android-app directory

--- android-app/keyboard_heliboard/app/src/main/java/helium314/keyboard/latin/syncmesh/SyncMeshClipboardItem.java
+++
@@ -1,8 +0,0 @@
-package helium314.keyboard.latin.syncmesh;
-
-public class SyncMeshClipboardItem {
-    public String text;
-    public String direction;
-    public String sourceDeviceName;
-    public long createdAt;
-}
